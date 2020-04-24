a117e1e6309b0ad81f29ec85cb8d610882cd0172
Rodrigo <rodrigoluis@gmail.com>
2020-04-24 17:47:09 -0300

    Geometry - Load External Object

--- src/geometriesExternalObjects.js
+++ src/geometriesExternalObjects.js
@@ -3,7 +3,7 @@ function main()
   var scene = new THREE.Scene();    // Create main scene
   var clock = new THREE.Clock();
   var stats = initStats();          // To show FPS information
-  var light = initDefaultLighting(scene, new THREE.Vector3(1, 2, 1)); // Use default light
+  var light = initDefaultLighting(scene, new THREE.Vector3(1, 2, 0.8)); // Use default light
   var lightSphere = createSphere(0.1, 10, 10);
     lightSphere.position.copy(light.position);
   scene.add(lightSphere);
@@ -12,8 +12,11 @@ function main()
     renderer.setClearColor("rgb(30, 30, 42)");
   var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.lookAt(0, 0, 0);
-    camera.position.set(0,1.5,3.0);
+    camera.position.set(2.18, 1.62, 3.31);
     camera.up.set( 0, 1, 0 );
+
+  // Control the appearence of first object loaded
+  var firstRender = false;
 
   // To use the keyboard
   var keyboard = new KeyboardState();
@@ -43,18 +46,68 @@ function main()
   var objectArray = new Array();
   var activeObject = 0; // View first object
 
-  loadOBJFile('../assets/objects/', 'dolphins', true);
-  loadOBJFile('../assets/objects/', 'rose+vase', false);
-  loadGLTFFile('../assets/objects/', 'TocoToucan', false);
-  loadOBJFile('../assets/objects/', 'f-16', false);
-  loadOBJFile('../assets/objects/', 'flowers', false);
-  loadOBJFile('../assets/objects/', 'porsche', false);
-  loadOBJFile('../assets/objects/', 'soccerball', false);
-  loadOBJFile('../assets/objects/', 'bunny', false);
+  loadOBJFile('../assets/objects/', 'dolphins', false, 1.5);
+  loadOBJFile('../assets/objects/', 'rose+vase', false, 1.5);
+  loadGLTFFile('../assets/objects/', 'TocoToucan', false, 2.0);
+  loadFBXFile('../assets/objects/', 'raptor', false, 2.5);
+  loadPLYFile('../assets/objects/', 'cow', false, 2.0);
+  loadOBJFile('../assets/objects/', 'f-16', false, 2.2);
+  loadOBJFile('../assets/objects/', 'flowers', false, 1.5);
+  loadOBJFile('../assets/objects/', 'soccerball', false, 1.2);
 
   render();
 
-  function loadGLTFFile(modelPath, modelName, visibility)
+  function loadPLYFile(modelPath, modelName, visibility, desiredScale)
+  {
+    var loader = new THREE.PLYLoader( );
+    loader.load( modelPath + modelName + '.ply', function ( geometry ) {
+
+      geometry.computeVertexNormals();
+
+      var material = new THREE.MeshPhongMaterial({color:"rgb(255,120,50)"});
+      var obj = new THREE.Mesh( geometry, material );
+
+      obj.name = modelName;
+      obj.visible = visibility;
+      obj.castShadow = true;
+
+      var obj = normalizeAndRescale(obj, desiredScale);
+      var obj = fixPosition(obj);
+
+      scene.add( obj );
+      objectArray.push( obj );
+
+			}, onProgress, onError);
+  }
+
+  function loadFBXFile(modelPath, modelName, visibility, desiredScale)
+  {
+    var loader = new THREE.FBXLoader( );
+    loader.load( modelPath + modelName + '.fbx', function ( object ) {
+      var obj = object;
+      obj.name = modelName;
+      obj.visible = visibility;
+      obj.traverse( function ( child ) {
+      	if ( child ) {
+           child.castShadow = true;
+      	}
+      });
+      obj.traverse( function( node )
+      {
+        if( node.material ) node.material.side = THREE.DoubleSide;
+      });
+
+      var obj = normalizeAndRescale(obj, desiredScale);
+      var obj = fixPosition(obj);
+
+      scene.add ( obj );
+      objectArray.push( obj );
+
+			}, onProgress, onError);
+  }
+
+
+  function loadGLTFFile(modelPath, modelName, visibility, desiredScale)
   {
     var loader = new THREE.GLTFLoader( );
     loader.load( modelPath + modelName + '.gltf', function ( gltf ) {
@@ -71,23 +124,19 @@ function main()
         if( node.material ) node.material.side = THREE.DoubleSide;
       });
 
-      var objectFixed = fixScaleAndPosition(obj);
-      scene.add ( objectFixed );
-      objectArray.push( objectFixed );
-			} );
+      var obj = normalizeAndRescale(obj, desiredScale);
+      var obj = fixPosition(obj);
+
+      scene.add ( obj );
+      objectArray.push( obj );
+
+			}, onProgress, onError);
   }
 
 
-  function loadOBJFile(modelPath, modelName, visibility)
+  function loadOBJFile(modelPath, modelName, visibility, desiredScale)
   {
-    var onProgress = function ( xhr ) {
-       if ( xhr.lengthComputable ) {
-         var percentComplete = xhr.loaded / xhr.total * 100;
-         infoBox.changeMessage( modelName + ": "+ Math.round( percentComplete, 2 ) + '% processed' );
-       }
-    }
-
-    var onError = function () { };
+    currentModel = modelName;
 
     var manager = new THREE.LoadingManager( );
 
@@ -113,21 +162,36 @@ function main()
              if( node.material ) node.material.side = THREE.DoubleSide;
            });
 
-           var objectFixed = fixScaleAndPosition(obj);
+           var obj = normalizeAndRescale(obj, desiredScale);
+           var obj = fixPosition(obj);
 
-           scene.add ( objectFixed );
-           objectArray.push( objectFixed );
+           scene.add ( obj );
+           objectArray.push( obj );
          }, onProgress, onError );
     });
   }
 
-  function fixScaleAndPosition(obj)
+  function onError() { };
+
+  function onProgress ( xhr, model ) {
+     if ( xhr.lengthComputable ) {
+       var percentComplete = xhr.loaded / xhr.total * 100;
+       infoBox.changeMessage("Loading... " + Math.round( percentComplete, 2 ) + '% processed' );
+     }
+  }
+
+  // Normalize scale and multiple by the newScale
+  function normalizeAndRescale(obj, newScale)
   {
-    // Normalize
     var scale = getMaxSize(obj); // Available in 'utils.js'
+    obj.scale.set(newScale * (1.0/scale),
+                  newScale * (1.0/scale),
+                  newScale * (1.0/scale));
+    return obj;
+  }
 
-    obj.scale.set(1/scale, 1/scale, 1/scale);
-
+  function fixPosition(obj)
+  {
    // Fix position of the object over the ground plane
     var box = new THREE.Box3().setFromObject( obj );
     if(box.min.y > 0)
@@ -137,6 +201,13 @@ function main()
     return obj;
   }
 
+  function renderFirstObjectLoaded()
+  {
+    activeObject = 0;
+    objectArray[0].visible = true;
+    if(!firstRender) firstRender = true;
+  }
+
   function keyboardUpdate()
   {
     keyboard.update();
@@ -144,8 +215,19 @@ function main()
     {
       axesHelper.visible = !axesHelper.visible
     }
+    if ( keyboard.down("enter"))
+    {
+      if(activeObject != 0) objectArray[activeObject].visible = false;
+      renderFirstObjectLoaded();
+      infoBox.changeMessage("Object " + activeObject + ": " + objectArray[activeObject].name);
+    }
     if ( keyboard.down("right") )
     {
+      if(!firstRender)
+      {
+        renderFirstObjectLoaded();
+        return;
+      }
       activeObject++;
       if(activeObject < objectArray.length)
       {
@@ -161,6 +243,11 @@ function main()
     }
     if ( keyboard.down("left") )
     {
+      if(!firstRender)
+      {
+        renderFirstObjectLoaded();
+        return;
+      }
       activeObject--;
       if(activeObject < 0)
       {
@@ -183,6 +270,7 @@ function main()
       controls.add("External Loader Example");
       controls.show();
       controls.addParagraph();
+      controls.add("Pressione 'ENTER' para mostrar o primeiro objeto carregado.");
       controls.add("Pressione 'A' para visualizar/ocultar os eixos.");
       controls.add("Pressione as setas para direita e esquerda para alterar o objeto.");
       controls.show();
