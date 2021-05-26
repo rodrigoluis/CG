655032ade21d8bf9806fd38988f20b61d248c944
renanNun <renannunes400@gmail.com>
2021-05-25 23:25:43 -0300

    Adicionando aula 01

---
+++ works/aula01/aula01_ex01.js
@@ -0,0 +1,67 @@
+import * as THREE from  '../build/three.module.js';
+import Stats from       '../build/jsm/libs/stats.module.js';
+import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
+import {initRenderer, 
+        initCamera,
+        InfoBox,
+        onWindowResize} from "../libs/util/util.js";
+
+var stats = new Stats();          // To show FPS information
+var scene = new THREE.Scene();    // Create main scene
+var renderer = initRenderer();    // View function in util/utils
+var camera = initCamera(new THREE.Vector3(0, -30, 15)); // Init camera in this position
+
+// Enable mouse rotation, pan, zoom etc.
+var trackballControls = new TrackballControls( camera, renderer.domElement );
+
+// Show axes (parameter is size of each axis)
+var axesHelper = new THREE.AxesHelper( 12 );
+scene.add( axesHelper );
+
+// create the ground plane
+var planeGeometry = new THREE.PlaneGeometry(20, 20);
+planeGeometry.translate(0.0, 0.0, -0.02); // To avoid conflict with the axeshelper
+var planeMaterial = new THREE.MeshBasicMaterial({
+    color: "rgba(150, 150, 150)",
+    side: THREE.DoubleSide,
+});
+var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+// add the plane to the scene
+scene.add(plane);
+
+// create a cube
+//var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+var cubeMaterial = new THREE.MeshNormalMaterial();
+var cube_01 = new THREE.Mesh(new THREE.BoxGeometry(4, 4, 4), cubeMaterial);
+var cube_02 = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), cubeMaterial);
+var cube_03 = new THREE.Mesh(new THREE.BoxGeometry(6, 6, 6), cubeMaterial);
+// position the cube
+cube_01.position.set(0.0, 0.0, 10.0);
+cube_02.position.set(0.0, 10.0, 1.0);
+cube_03.position.set(10.0, 0.0, 3.0);
+// add the cube to the scene
+scene.add(cube_01);
+scene.add(cube_02);
+scene.add(cube_03);
+
+// Use this to show information onscreen
+var controls = new InfoBox();
+  controls.add("Basic Scene");
+  controls.addParagraph();
+  controls.add("Use mouse to interact:");
+  controls.add("* Left button to rotate");
+  controls.add("* Right button to translate (pan)");
+  controls.add("* Scroll to zoom in/out.");
+  controls.show();
+
+// Listen window size changes
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+
+render();
+function render()
+{
+  stats.update(); // Update FPS
+  trackballControls.update(); // Enable mouse movements
+  requestAnimationFrame(render);
+  renderer.render(scene, camera) // Render scene
+}
---
+++ works/aula01/aula01_ex03.js
@@ -0,0 +1,72 @@
+import * as THREE from  '../build/three.module.js';
+import Stats from       '../build/jsm/libs/stats.module.js';
+import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
+import {initRenderer, 
+        initCamera,
+        InfoBox,
+        onWindowResize} from "../libs/util/util.js";
+
+var stats = new Stats();          // To show FPS information
+var scene = new THREE.Scene();    // Create main scene
+var renderer = initRenderer();    // View function in util/utils
+var camera = initCamera(new THREE.Vector3(0, -30, 15)); // Init camera in this position
+
+// Enable mouse rotation, pan, zoom etc.
+var trackballControls = new TrackballControls( camera, renderer.domElement );
+
+// Show axes (parameter is size of each axis)
+var axesHelper = new THREE.AxesHelper( 12 );
+scene.add( axesHelper );
+
+// create the ground plane
+var planeGeometry = new THREE.PlaneGeometry(20, 20);
+planeGeometry.translate(0.0, 0.0, -0.02); // To avoid conflict with the axeshelper
+var planeMaterial = new THREE.MeshBasicMaterial({
+    color: "rgba(150, 150, 150)",
+    side: THREE.DoubleSide,
+});
+var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+// add the plane to the scene
+scene.add(plane);
+
+//var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+var cubeMaterial = new THREE.MeshNormalMaterial();
+var tam = 9;
+var array = [-1,-2,-3,-4,0,1,2,3,4].sort(() => .5 - Math.random());
+for(let i = 0; i < tam; i++){
+  let cubeSize = array[i];
+  var cube = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), new THREE.MeshNormalMaterial());
+  cube.position.set(cubeSize * i ,cubeSize, 2.0);
+  scene.add(cube);
+}
+
+
+//console.log(cube.length);
+// position the cube
+
+
+// add the cube to the scene
+//
+
+
+// Use this to show information onscreen
+var controls = new InfoBox();
+  controls.add("Basic Scene");
+  controls.addParagraph();
+  controls.add("Use mouse to interact:");
+  controls.add("* Left button to rotate");
+  controls.add("* Right button to translate (pan)");
+  controls.add("* Scroll to zoom in/out.");
+  controls.show();
+
+// Listen window size changes
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+
+render();
+function render()
+{
+  stats.update(); // Update FPS
+  trackballControls.update(); // Enable mouse movements
+  requestAnimationFrame(render);
+  renderer.render(scene, camera) // Render scene
+}
