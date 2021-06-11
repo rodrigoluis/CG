0f47131028a6ce2218d801d6cc424d9b5e4b1344
Rodrigo <rodrigoluis@gmail.com>
2021-06-11 16:13:11 -0300

    water test (temp)

---
+++ works/webxr_VR_FreeMove.js
@@ -0,0 +1,239 @@
+//-- Imports -------------------------------------------------------------------------------------
+import * as THREE from '../build/three.module.js';
+import { VRButton } from '../build/jsm/webxr/VRButton.js';
+import { Water } from '../assets/shaders/Water.js';
+import { Sky } from '../assets/shaders/Sky.js';
+//import Stats from '../build/jsm/libs/stats.module.js';
+import {onWindowResize} from "../libs/util/util.js";
+
+//-----------------------------------------------------------------------------------------------
+//-- MAIN SCRIPT --------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+
+
+//--  General globals ---------------------------------------------------------------------------
+window.addEventListener( 'resize', onWindowResize );
+//let container = document.getElementById( 'container' );
+//container.appendChild( renderer.domElement );
+
+//let stats = new Stats();
+//container.appendChild( stats.dom );
+let water, sun, mesh;
+
+//-- Renderer settings ---------------------------------------------------------------------------
+let renderer = new THREE.WebGLRenderer();
+	renderer.setClearColor(new THREE.Color("rgb(70, 150, 240)"));
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.xr.enabled = true;
+	renderer.outputEncoding = THREE.sRGBEncoding;
+	renderer.shadowMap.enabled = true;
+
+//-- Setting scene and camera -------------------------------------------------------------------
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, .1, 1000 );
+let moveCamera; // Move when a button is pressed 
+
+//-- 'Camera Holder' to help moving the camera
+const cameraHolder = new THREE.Object3D();
+cameraHolder.add(camera);
+scene.add( cameraHolder );
+//-- Create VR button and settings ---------------------------------------------------------------
+document.body.appendChild( VRButton.createButton( renderer ) );
+
+// controllers
+var controller1 = renderer.xr.getController( 0 );
+	controller1.addEventListener( 'selectstart', onSelectStart );
+	controller1.addEventListener( 'selectend', onSelectEnd );
+camera.add( controller1 );
+
+//-- Creating Scene and calling the main loop ----------------------------------------------------
+createScene();
+animate();
+
+//------------------------------------------------------------------------------------------------
+//-- FUNCTIONS -----------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+
+function move()
+{
+	if(moveCamera)
+	{
+		// Get Camera Rotation
+		let quaternion = new THREE.Quaternion();
+		quaternion = camera.quaternion;
+
+		// Get direction to translate from quaternion
+		var moveTo = new THREE.Vector3(0, 0, -0.1);
+		moveTo.applyQuaternion(quaternion);
+
+		// Move the camera Holder to the computed direction
+		cameraHolder.translateX(moveTo.x);
+		cameraHolder.translateY(moveTo.y);
+		cameraHolder.translateZ(moveTo.z);	
+	}
+}
+
+function onSelectStart( ) 
+{
+	moveCamera = true;
+}
+
+function onSelectEnd( ) 
+{
+	moveCamera = false;
+}
+
+//-- Main loop -----------------------------------------------------------------------------------
+function animate() 
+{
+	renderer.setAnimationLoop( render );
+}
+
+function render() {
+	move();
+	const time = performance.now() * 0.001;
+
+	mesh.position.y = Math.sin( time ) * 20 + 5;
+	mesh.rotation.x = time * 0.5;
+	mesh.rotation.z = time * 0.51;
+
+	water.material.uniforms[ 'time' ].value += 1.0 / 60.0;	
+	//stats.update();	
+	renderer.render( scene, camera );
+}
+
+//------------------------------------------------------------------------------------------------
+//-- Scene and auxiliary functions ---------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+
+//-- Create Scene --------------------------------------------------------------------------------
+function createScene()
+{
+				//
+
+				//let water, sun, mesh;
+				sun = new THREE.Vector3();
+
+				// Water
+
+				const waterGeometry = new THREE.PlaneGeometry( 10000, 10000 );
+
+				water = new Water(
+					waterGeometry,
+					{
+						textureWidth: 512,
+						textureHeight: 512,
+						waterNormals: new THREE.TextureLoader().load( '../assets/textures/waternormals.jpg', function ( texture ) {
+
+							texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+						} ),
+						sunDirection: new THREE.Vector3(),
+						sunColor: 0xffffff,
+						waterColor: 0x001e0f,
+						distortionScale: 3.7,
+						fog: scene.fog !== undefined
+					}
+				);
+
+				water.rotation.x = - Math.PI / 2;
+
+				scene.add( water );
+
+				// Skybox
+
+				const sky = new Sky();
+				sky.scale.setScalar( 10000 );
+				scene.add( sky );
+
+				const skyUniforms = sky.material.uniforms;
+
+				skyUniforms[ 'turbidity' ].value = 10;
+				skyUniforms[ 'rayleigh' ].value = 2;
+				skyUniforms[ 'mieCoefficient' ].value = 0.005;
+				skyUniforms[ 'mieDirectionalG' ].value = 0.8;
+
+				const parameters = {
+					inclination: 0.49,
+					azimuth: 0.205
+				};
+
+				const pmremGenerator = new THREE.PMREMGenerator( renderer );
+
+				function updateSun() {
+
+					const theta = Math.PI * ( parameters.inclination - 0.5 );
+					const phi = 2 * Math.PI * ( parameters.azimuth - 0.5 );
+
+					sun.x = Math.cos( phi );
+					sun.y = Math.sin( phi ) * Math.sin( theta );
+					sun.z = Math.sin( phi ) * Math.cos( theta );
+
+					sky.material.uniforms[ 'sunPosition' ].value.copy( sun );
+					water.material.uniforms[ 'sunDirection' ].value.copy( sun ).normalize();
+
+					scene.environment = pmremGenerator.fromScene( sky ).texture;
+
+				}
+
+				updateSun();
+
+				//
+
+				const geometry = new THREE.BoxGeometry( 30, 30, 30 );
+				const material = new THREE.MeshStandardMaterial( { roughness: 0 } );
+
+				mesh = new THREE.Mesh( geometry, material );
+				scene.add( mesh );
+
+				//	
+/*	// Light stuff 
+	const light = new THREE.PointLight(0xaaaaaa);
+		light.position.set(30,30,20);
+		light.castShadow = true;
+		light.distance = 0;
+		light.shadow.mapSize.width = 1024;
+		light.shadow.mapSize.height = 1024;	
+	scene.add(light);
+
+	var ambientLight = new THREE.AmbientLight(0x121212);
+		scene.add(ambientLight);
+
+	// Load all textures 
+	var textureLoader = new THREE.TextureLoader();
+		var floor 	= textureLoader.load('../assets/textures/sand.jpg');	
+		var cubeTex = textureLoader.load('../assets/textures/crate.jpg');			
+
+	// Create Ground Plane
+	var groundPlane = createGroundPlane(80.0, 80.0, 100, 100, "rgb(200,200,150)");
+		groundPlane.rotateX(degreesToRadians(-90));
+		groundPlane.material.map = floor;		
+		groundPlane.material.map.wrapS = THREE.RepeatWrapping;
+		groundPlane.material.map.wrapT = THREE.RepeatWrapping;
+		groundPlane.material.map.repeat.set(8,8);		
+	scene.add(groundPlane);
+
+	// Create feature cubes [size, xPos, zPos, textureName]
+	createCube(3.0, -20.0, -20.0, cubeTex);
+	createCube(1.0, -15.0,  12.0, cubeTex);
+	createCube(1.0, -10.0,  -5.0, cubeTex);
+	createCube(1.0,  -5.0,  13.0, cubeTex);
+	createCube(1.0,   5.0,  10.0, cubeTex);
+	createCube(1.0,  10.0, -15.0, cubeTex);
+	createCube(1.0,  20.0, -12.0, cubeTex);
+	createCube(4.0,  20.0,  22.0, cubeTex);
+	*/		
+}
+
+function createCube(cubeSize, xPos, zPos, texture)
+{
+	var cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+	var cubeMaterial = new THREE.MeshLambertMaterial();
+	var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+		cube.castShadow = true;
+		cube.receiveShadow = true;		
+		cube.position.set(xPos, cubeSize/2.0, zPos);
+		cube.material.map = texture;
+	scene.add(cube);	
+}
