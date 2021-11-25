af0a71ffb07588ddfb193fd9ccf4d7c555bbec18
rodrigo <rodrigoluis@gmail.com>
2021-11-25 15:55:58 -0300

    Adding Joystick examples

--- src/exampleJoystick.js
+++ src/exampleJoystick.js
@@ -10,14 +10,14 @@ import {initRenderer,
         degreesToRadians} from "../libs/util/util.js";      
         
 // vars
+let scale = 1;
+let size = 5;
 let fwdValue = 0;
 let bkdValue = 0;
 let rgtValue = 0;
 let lftValue = 0;
 let tempVector = new THREE.Vector3();
 let upVector = new THREE.Vector3(0, 1, 0);
-let joyManager;
-
 
 // Create a renderer and add it to the DOM.
 var scene = new THREE.Scene();
@@ -39,14 +39,15 @@ var groundPlane = createGroundPlaneWired(200, 200, 40, 40); // width and height
 scene.add(groundPlane);
 
 // create the inner cube
-var geometry = new THREE.BoxGeometry(5, 5, 5);
+var geometry = new THREE.BoxGeometry(size, size, size);
 var material = new THREE.MeshLambertMaterial({color:"rgb(255,0,0)"});
 var mesh = new THREE.Mesh(geometry, material);
-  mesh.position.y = 2.5;
+  mesh.position.y = size/2.0;
+  mesh.scale.set(scale, scale, scale);
 scene.add(mesh);
 
 render();
-addJoystick();
+addJoysticks();
 
 // Renders the scene
 function render() {
@@ -101,6 +102,9 @@ function updatePlayer(){
       }
   
   mesh.updateMatrixWorld()
+
+  mesh.scale.set(scale, scale, scale);
+  mesh.position.y = 5 * scale /2.0;
   
   //controls.target.set( mesh.position.x, mesh.position.y, mesh.position.z );
   // reposition camera
@@ -109,47 +113,57 @@ function updatePlayer(){
   camera.position.add(mesh.position)
 };
 
-function addJoystick(){
-    const options = {
-        zone: document.getElementById('joystickWrapper1'),
-        size: 120,
-        multitouch: true,
-        maxNumberOfNipples: 2,
-        mode: 'static',
-        restJoystick: true,
-        shape: 'circle',
-        // position: { top: 20, left: 20 },
-        position: { top: '160px', left: '60px' },
-        dynamicPage: true,
-      }  
-    
-  joyManager = nipplejs.create(options);
+function addJoysticks(){
+   
+  // Details in the link bellow:
+  // https://yoannmoi.net/nipplejs/
+
+  let joystickL = nipplejs.create({
+    zone: document.getElementById('joystickWrapper1'),
+    mode: 'static',
+    position: { top: '160px', left: '60px' }
+  });
   
-  joyManager['0'].on('move', function (evt, data) {
-        const forward = data.vector.y
-        const turn = data.vector.x
+  joystickL.on('move', function (evt, data) {
+    const forward = data.vector.y
+    const turn = data.vector.x
 
-        if (forward > 0) {
-          fwdValue = Math.abs(forward)
-          bkdValue = 0
-        } else if (forward < 0) {
-          fwdValue = 0
-          bkdValue = Math.abs(forward)
-        }
+    if (forward > 0) {
+      fwdValue = Math.abs(forward)
+      bkdValue = 0
+    } else if (forward < 0) {
+      fwdValue = 0
+      bkdValue = Math.abs(forward)
+    }
 
-        if (turn > 0) {
-          lftValue = 0
-          rgtValue = Math.abs(turn)
-        } else if (turn < 0) {
-          lftValue = Math.abs(turn)
-          rgtValue = 0
-        }
-      })
+    if (turn > 0) {
+      lftValue = 0
+      rgtValue = Math.abs(turn)
+    } else if (turn < 0) {
+      lftValue = Math.abs(turn)
+      rgtValue = 0
+    }
+  })
 
-      joyManager['0'].on('end', function (evt) {
-        bkdValue = 0
-        fwdValue = 0
-        lftValue = 0
-        rgtValue = 0
-      })
+  joystickL.on('end', function (evt) {
+    bkdValue = 0
+    fwdValue = 0
+    lftValue = 0
+    rgtValue = 0
+  })
+
+  let joystickR = nipplejs.create({
+    zone: document.getElementById('joystickWrapper2'),
+    mode: 'static',
+    lockY: true,                 // only move on the Y axis
+    position: { top: '160px', right: '60px' },
+  });
+
+  joystickR.on('move', function (evt, data) {
+    const localScale = data.vector.y;
+
+    scale += localScale;
+    if(scale > 3.0) scale = 3.0;
+    if(scale < 0.5) scale = 0.5;
+  })
 }
