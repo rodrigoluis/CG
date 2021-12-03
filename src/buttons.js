efe557f12001d57382f3ae572293903640a9a2e1
rodrigo <rodrigoluis@gmail.com>
2021-12-03 15:36:55 -0300

    Testing buttons

--- src/buttons.js
+++ src/buttons.js
@@ -10,8 +10,8 @@ import {initRenderer,
 var pressed = false;        
 var buttons = document.getElementsByTagName("button");
 for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("mousedown", onButtonClick, false);
-  buttons[i].addEventListener("mouseup", onButtonUp, false);  
+  buttons[i].addEventListener("touchstart", onButtonClick, false);
+  buttons[i].addEventListener("touchend", onButtonUp, false);  
 };
 
 function onButtonClick(event) {
