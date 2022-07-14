58b4198dccb930009766820ab82872caac185e40
rodrigo <rodrigoluis@gmail.com>
2022-07-14 17:51:37 -0300

    update external objects examples

--- examples/geometriesExternalObjects.js
+++ examples/geometriesExternalObjects.js
@@ -18,10 +18,11 @@ scene = new THREE.Scene();    // Create main scene
 renderer = initRenderer();    // View function in util/utils
    renderer.setClearColor("rgb(30, 30, 42)");
 camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-   camera.lookAt(0, 0, 0);
-   camera.position.set(2.18, 1.62, 3.31);
-   camera.up.set( 0, 1, 0 );
+   camera.position.set(2.18, 2, 3.31);
 orbit = new OrbitControls( camera, renderer.domElement ); // Enable mouse rotation, pan, zoom etc.
+   orbit.target.set(0, .5, 0);
+   orbit.update();
+
 light = initDefaultBasicLight(scene, true); // Use default light
 
 // Listen window size changes
@@ -40,21 +41,43 @@ scene.add( axesHelper );
 
 //---------------------------------------------------------
 // Load external objects
+
+// Assets manager --------------------------------
 let assets = {
+   // Properties ---------------------------------
    dolphins: null,
    vase: null,
    flowers: null,
    toucan: null,
    cow: null,
    f16: null,
-   ball: null,
-   allLoaded: false
+   soccerball: null,
+
+   allLoaded: false, 
+
+   // Functions ----------------------------------
+   checkLoaded : function() {
+      if(!this.allLoaded)
+      {
+         if(this.dolphins && this.vase && this.flowers && 
+            this.f16 && this.soccerball && this.toucan && this.cow){
+             this.allLoaded = true;
+             loadingMessage.hide(); 
+         }
+      }
+   },   
+
+   hideAll : function() {
+      this.dolphins.visible = this.vase.visible = this.flowers.visible = false;
+      this.f16.visible = this.soccerball.visible = this.toucan.visible = 
+      this.cow.visible = false;
+   }
 }
 
 loadOBJFile('../assets/objects/', 'dolphins', true, 1.5);
-loadOBJFile('../assets/objects/', 'rose+vase', false, 1.5);
+loadOBJFile('../assets/objects/', 'vase', false, 1.5);
 loadOBJFile('../assets/objects/', 'flowers', false, 1.5);
-loadGLBFile('../assets/objects/', 'TocoToucan', false, 2.0);
+loadGLBFile('../assets/objects/', 'toucan', false, 2.0);
 loadPLYFile('../assets/objects/', 'cow', false, 2.0);
 loadOBJFile('../assets/objects/', 'f16', false, 2.2);
 loadOBJFile('../assets/objects/', 'soccerball', false, 1.2);
@@ -66,48 +89,45 @@ function loadPLYFile(modelPath, modelName, visibility, desiredScale)
 {
   var loader = new PLYLoader( );
   loader.load( modelPath + modelName + '.ply', function ( geometry ) {
+      geometry.computeVertexNormals();
 
-    geometry.computeVertexNormals();
+      var material = new THREE.MeshPhongMaterial({color:"rgb(255,120,50)"});
+      var obj = new THREE.Mesh( geometry, material );
 
-    var material = new THREE.MeshPhongMaterial({color:"rgb(255,120,50)"});
-    var obj = new THREE.Mesh( geometry, material );
+      obj.name = modelName;
+      obj.visible = visibility;
+      obj.castShadow = true;
 
-    obj.name = modelName;
-    obj.visible = visibility;
-    obj.castShadow = true;
+      var obj = normalizeAndRescale(obj, desiredScale);
+      var obj = fixPosition(obj);
 
-    var obj = normalizeAndRescale(obj, desiredScale);
-    var obj = fixPosition(obj);
-
-    scene.add( obj );
-
-    if(modelName == 'cow') assets.cow = obj;            
+      scene.add( obj );
+      assets[modelName] = obj;     
     });
 }
 
 function loadGLBFile(modelPath, modelName, visibility, desiredScale)
 {
-  var loader = new GLTFLoader( );
-  loader.load( modelPath + modelName + '.glb', function ( gltf ) {
-    var obj = gltf.scene;
-    obj.name = modelName;
-    obj.visible = visibility;
-    obj.traverse( function ( child ) {
-      if ( child ) {
-          child.castShadow = true;
-      }
-    });
-    obj.traverse( function( node )
-    {
-      if( node.material ) node.material.side = THREE.DoubleSide;
-    });
+   var loader = new GLTFLoader( );
+   loader.load( modelPath + modelName + '.glb', function ( gltf ) {
+      var obj = gltf.scene;
+      obj.name = modelName;
+      obj.visible = visibility;
+      obj.traverse( function ( child ) {
+         if ( child ) {
+            child.castShadow = true;
+         }
+      });
+      obj.traverse( function( node )
+      {
+         if( node.material ) node.material.side = THREE.DoubleSide;
+      });
 
-    var obj = normalizeAndRescale(obj, desiredScale);
-    var obj = fixPosition(obj);
+      var obj = normalizeAndRescale(obj, desiredScale);
+      var obj = fixPosition(obj);
 
-    scene.add ( obj );
-
-    if(modelName == 'TocoToucan') assets.toucan = obj;            
+      scene.add ( obj );
+      assets[modelName] = obj;        
     });
 }
 
@@ -125,27 +145,21 @@ function loadOBJFile(modelPath, modelName, visibility, desiredScale)
       objLoader.load( modelName + ".obj", function ( obj ) {
          obj.name = modelName;
          obj.visible = visibility;
-         // Set 'castShadow' property for each children of the group
          obj.traverse( function (child)
          {
-         child.castShadow = true;
+            child.castShadow = true;
          });
 
          obj.traverse( function( node )
          {
-         if( node.material ) node.material.side = THREE.DoubleSide;
+            if( node.material ) node.material.side = THREE.DoubleSide;
          });
 
          var obj = normalizeAndRescale(obj, desiredScale);
          var obj = fixPosition(obj);
 
          scene.add ( obj );
-      
-         if(modelName == 'dolphins')   assets.dolphins = obj;         
-         if(modelName == 'rose+vase')  assets.vase = obj;         
-         if(modelName == 'flowers')    assets.flowers = obj;         
-         if(modelName == 'f16')        assets.f16 = obj;                                    
-         if(modelName == 'soccerball') assets.ball = obj;
+         assets[modelName] = obj;
       });
   });
 
@@ -154,7 +168,7 @@ function loadOBJFile(modelPath, modelName, visibility, desiredScale)
 // Normalize scale and multiple by the newScale
 function normalizeAndRescale(obj, newScale)
 {
-  var scale = getMaxSize(obj); // Available in 'utils.js'
+  var scale = getMaxSize(obj); 
   obj.scale.set(newScale * (1.0/scale),
                 newScale * (1.0/scale),
                 newScale * (1.0/scale));
@@ -172,26 +186,17 @@ function fixPosition(obj)
   return obj;
 }
 
-
-
 function buildInterface()
 {
   // Interface
   var controls = new function ()
   {
     this.viewAxes = false;
-    this.type = "Dolphins";
+    this.type = "dolphins";
     this.onChooseObject = function()
     {
-      hideAllAssets();
-      if(this.type == 'Dolphins')   assets.dolphins.visible = true;
-      if(this.type == 'Vase')       assets.vase.visible = true;      
-      if(this.type == 'Flowers')    assets.flowers.visible = true;
-      if(this.type == 'F16')        assets.f16.visible = true;      
-      if(this.type == 'Soccerball') assets.ball.visible = true;
-      if(this.type == 'TocoToucan') assets.toucan.visible = true;      
-      if(this.type == 'Cow')        assets.cow.visible = true;
-
+      assets.hideAll();
+      assets[this.type].visible = true; 
     };
     this.onViewAxes = function(){
        axesHelper.visible = this.viewAxes;
@@ -201,8 +206,7 @@ function buildInterface()
   // GUI interface
   var gui = new GUI();
   gui.add(controls, 'type',
-    ['Dolphins', 'Vase', 'Flowers', 'F16',
-      'Soccerball', 'TocoToucan', 'Cow'])
+    ['dolphins', 'vase', 'flowers', 'f16', 'soccerball', 'toucan', 'cow'])
     .name("Change Object")
     .onChange(function(e) { controls.onChooseObject(); });
   gui.add(controls, 'viewAxes', false)
@@ -210,29 +214,9 @@ function buildInterface()
     .onChange(function(e) { controls.onViewAxes() });    
 }
 
-function hideAllAssets()
-{
-   assets.dolphins.visible = assets.vase.visible = assets.flowers.visible = 
-   assets.f16.visible = assets.ball.visible = 
-   assets.toucan.visible = assets.cow.visible = false;
-}
-
-function checkLoaded()
-{
-   if(!assets.allLoaded)
-   {
-      if(assets.dolphins && assets.vase && assets.flowers && 
-         assets.f16 && assets.ball && assets.toucan && assets.cow){
-         assets.allLoaded = true;
-         loadingMessage.hide(); 
-      }
-   }
-}
-
-
 function render()
 {
-   checkLoaded();
+   assets.checkLoaded();
    requestAnimationFrame(render);
    renderer.render(scene, camera)
 }
--- examples/textureExternalObjects.js
+++ examples/textureExternalObjects.js
@@ -17,12 +17,13 @@ let scene, renderer, camera, orbit, light;
 scene = new THREE.Scene();    // Create main scene
 light = initDefaultSpotlight(scene, new THREE.Vector3(2, 3, 2)); // Use default light
 renderer = initRenderer();    // View function in util/utils
-  renderer.setClearColor("rgb(30, 30, 42)");
+   renderer.setClearColor("rgb(30, 30, 42)");
 camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.lookAt(0, 0, 0);
-  camera.position.set(2.18, 1.62, 3.31);
-  camera.up.set( 0, 1, 0 );
+   camera.position.set(2.18, 1.62, 3.31);
+   camera.up.set( 0, 1, 0 );
 orbit = new OrbitControls( camera, renderer.domElement ); // Enable mouse rotation, pan, zoom etc.
+   orbit.target.set(0, 0.2, 0);
+   orbit.update();   
 
 let lightSphere = createSphere(0.1, 10, 10);
   lightSphere.position.copy(light.position);
@@ -44,23 +45,43 @@ scene.add( axesHelper );
 
 //---------------------------------------------------------
 // Load external objects
+
+// Assets manager --------------------------------
 let assets = {
+   // Properties ---------------------------------
    plane: null,
    L200: null,
    tank: null,
    orca: null,
    woodenGoose: null,
    chair: null,
-   allLoaded: false
+   allLoaded: false,
+
+   // Functions ----------------------------------
+   checkLoaded : function() {
+      if(!this.allLoaded)
+      {
+         if(this.L200 && this.chair && this.orca &&
+            this.plane && this.tank && this.woodenGoose){
+             this.allLoaded = true;
+             loadingMessage.hide(); 
+         }
+      }
+   },   
+
+   hideAll : function() {
+      this.orca.visible = this.woodenGoose.visible = this.chair.visible = 
+      this.plane.visible = this.L200.visible = this.tank.visible = false;
+   }
 }
 
-loadOBJFile('../assets/objects/', 'plane', 3.0, 0, true);
+loadOBJFile('../assets/objects/', 'plane', 3.5, 0, true);
 loadOBJFile('../assets/objects/', 'L200', 2.5, 90, false);
 loadOBJFile('../assets/objects/', 'tank', 2.0, 90, false);
 
-loadGLTFFile('../assets/objects/orca.glb', 4.0, 180, false);
-loadGLTFFile('../assets/objects/woodenGoose.glb', 2.0, 90, false);
-loadGLTFFile('../assets/objects/chair.glb', 1.0, 180, false);
+loadGLTFFile('../assets/objects/', 'orca', 4.0, 180, false);
+loadGLTFFile('../assets/objects/', 'woodenGoose', 2.0, 90, false);
+loadGLTFFile('../assets/objects/','chair', 1.5, 180, false);
 
 buildInterface();
 render();
@@ -94,40 +115,35 @@ function loadOBJFile(modelPath, modelName, desiredScale, angle, visibility)
         obj.rotateY(degreesToRadians(angle));
 
         scene.add ( obj );
-        if(modelName == 'plane') assets.plane = obj;         
-        if(modelName == 'L200')  assets.L200 = obj;         
-        if(modelName == 'tank')  assets.tank = obj;         
+        assets[modelName] = obj;        
       });
   });
 }
 
-function loadGLTFFile(modelName, desiredScale, angle, visibility)
+function loadGLTFFile(modelPath, modelName, desiredScale, angle, visibility)
 {
-  var loader = new GLTFLoader( );
-  loader.load( modelName, function ( gltf ) {
-    var obj = gltf.scene;
-    obj.visible = visibility;
-    obj.name = getFilename(modelName);
-    obj.traverse( function ( child ) {
+   var loader = new GLTFLoader( );
+   loader.load( modelPath + modelName + '.glb', function ( gltf ) {
+      var obj = gltf.scene;
+      obj.visible = visibility;
+      obj.name = getFilename(modelName);
+      obj.traverse( function ( child ) {
       if ( child ) {
-          child.castShadow = true;
+         child.castShadow = true;
       }
-    });
-    obj.traverse( function( node )
-    {
-      if( node.material ) node.material.side = THREE.DoubleSide;
-    });
+      });
+      obj.traverse( function( node )
+      {
+         if( node.material ) node.material.side = THREE.DoubleSide;
+      });
 
-    var obj = normalizeAndRescale(obj, desiredScale);
-    var obj = fixPosition(obj);
-    obj.rotateY(degreesToRadians(angle));
+      var obj = normalizeAndRescale(obj, desiredScale);
+      var obj = fixPosition(obj);
+      obj.rotateY(degreesToRadians(angle));
 
-    scene.add ( obj );
-    if(obj.name == 'orca.glb')        assets.orca = obj;         
-    if(obj.name == 'woodenGoose.glb') assets.woodenGoose = obj;         
-    if(obj.name == 'chair.glb')       assets.chair = obj; 
-
-    });
+      scene.add ( obj );
+      assets[modelName] = obj;
+   });
 }
 
 // Normalize scale and multiple by the newScale
@@ -160,28 +176,17 @@ function createSphere(radius, widthSegments, heightSegments)
   return object;
 }
 
-function hideAllAssets()
-{
-   assets.orca.visible = assets.woodenGoose.visible = assets.chair.visible = 
-   assets.plane.visible = assets.L200.visible = assets.tank.visible = false;
-}
-
 function buildInterface()
 {
   // Interface
   var controls = new function ()
   {
     this.viewAxes = false;
-    this.type = "Plane";
+    this.type = "plane";
     this.onChooseObject = function()
     {
-      hideAllAssets();
-      if(this.type == 'Orca')   assets.orca.visible = true;
-      if(this.type == 'Goose')  assets.woodenGoose.visible = true;      
-      if(this.type == 'Chair')  assets.chair.visible = true;
-      if(this.type == 'Plane')  assets.plane.visible = true;      
-      if(this.type == 'L200')   assets.L200.visible = true;
-      if(this.type == 'Tank')   assets.tank.visible = true;      
+      assets.hideAll();
+      assets[this.type].visible = true;   
     };
     this.onViewAxes = function(){
       axesHelper.visible = this.viewAxes;
@@ -191,29 +196,17 @@ function buildInterface()
   // GUI interface
   var gui = new GUI();
   gui.add(controls, 'type',
-  ['Orca', 'Goose', 'Chair', 'Plane', 'L200', 'Tank'])
+  ['orca', 'woodenGoose', 'chair', 'plane', 'L200', 'tank'])
      .name("Change Object")
-    .onChange(function(e) { controls.onChooseObject(); });
+     .onChange(function(e) { controls.onChooseObject(); });
   gui.add(controls, 'viewAxes', false)
-    .name("View Axes")
-    .onChange(function(e) { controls.onViewAxes() });
-}
-
-function checkLoaded()
-{
-   if(!assets.allLoaded)
-   {
-      if(assets.L200 && assets.chair && assets.orca &&
-         assets.plane && assets.tank && assets.woodenGoose){
-         assets.allLoaded = true;
-         loadingMessage.hide(); 
-      }
-   }
+     .name("View Axes")
+     .onChange(function(e) { controls.onViewAxes() });
 }
 
 function render()
 {
-   checkLoaded();
+   assets.checkLoaded();
    requestAnimationFrame(render);
    renderer.render(scene, camera)
 }
