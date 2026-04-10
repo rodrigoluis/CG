679954883c57a915489a84ccc0145a5b18f7bd3f
neonpi <ninaferreira534@gmail.com>
2026-04-10 15:46:22 -0300

    progresso no aviao

--- projetoAviao/aviao.js
+++ projetoAviao/aviao.js
@@ -1,14 +1,15 @@
 import * as THREE from  'three';
-import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
+import { OrbitControls } from '../../build/jsm/controls/OrbitControls.js';
+import KeyboardState from "../../libs/util/KeyboardState.js";
 import {initRenderer, 
         initCamera,
         initDefaultBasicLight,
         setDefaultMaterial,
         InfoBox,
         onWindowResize,
-        createGroundPlaneXZ} from "../libs/util/util.js";
+        createGroundPlaneXZ} from "../../libs/util/util.js";
 
-let scene, renderer, camera, material, light, orbit; // Initial variables
+let scene, renderer, camera, material, light, orbit ; // Initial variables
 scene = new THREE.Scene();    // Create main scene
 renderer = initRenderer();    // Init a basic renderer
 material = setDefaultMaterial(); // create a basic material
@@ -28,13 +29,49 @@ scene.add( axesHelper );
 let plane = createGroundPlaneXZ(20, 20)
 scene.add(plane);
 
-// create a cube
-let cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-let cube = new THREE.Mesh(cubeGeometry, material);
-// position the cube
-cube.position.set(0.0, 2.0, 0.0);
-// add the cube to the scene
-scene.add(cube);
+// criando materiais
+let materialRosa, materialBranco, materialDetalhe;
+materialRosa = setDefaultMaterial('rgb(233, 65,150)');
+materialBranco = setDefaultMaterial('branco');
+materialDetalhe = setDefaultMaterial('dark pink');
+
+//CorpoAviao
+const cilindroCorpo = new THREE.CylinderGeometry(3, 3, 10, 80);
+const cubeAsa = new THREE.ConeGeometry(4, 2, 2);
+const sphereoNariz = new THREE.SphereGeometry(3, 80);
+const sphereoRabo = new THREE.SphereGeometry(3, 80);
+const corpo = new THREE.Mesh(cilindroCorpo, materialRosa); 
+const nariz = new THREE.Mesh(sphereoNariz, materialBranco);
+const rabo = new THREE.Mesh(sphereoRabo, materialBranco);
+const basa = new THREE.Mesh(cubeAsa, materialBranco);
+let angle = THREE.MathUtils.degToRad(90);
+corpo.position.set(0, 5, 0);
+corpo.add(nariz);
+corpo.add(basa);
+corpo.add(rabo);
+nariz.position.set(0, 5, 0);
+basa.position.set(0, 10, 0);
+rabo.position.set(0, -5, 0);
+corpo.rotateX(angle);
+scene.add(corpo);
+
+//Controles basicos para teste
+var keyboard = new KeyboardState();
+function keyboardUpdate() {
+  keyboard.update();
+  if (keyboard.pressed("left")) corpo.translateX(-1);
+  if (keyboard.pressed("right")) corpo.translateX(1);
+  if (keyboard.pressed("up")) corpo.translateY(1);
+  if (keyboard.pressed("down")) corpo.translateY(-1);
+  if (keyboard.pressed("pageup")) corpo.translateZ(1);
+  if (keyboard.pressed("pagedown")) corpo.translateZ(-1);
+
+  let angle = THREE.MathUtils.degToRad(10);
+  if (keyboard.pressed("A")) corpo.rotateY(angle);
+  if (keyboard.pressed("D")) corpo.rotateY(-angle);
+}
+
+
 
 // Use this to show information onscreen
 let controls = new InfoBox();
@@ -49,6 +86,7 @@ let controls = new InfoBox();
 render();
 function render()
 {
+  keyboardUpdate();
   requestAnimationFrame(render);
   renderer.render(scene, camera) // Render scene
 }
